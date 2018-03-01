b3c06a5054185e63aa20a4a2a9f66d773f879477
d9h20f <>
2018-03-01 23:28:07 +0100

    Dutch translation

---
+++ translations/harbour-cargodock-nl.ts
@@ -0,0 +1,523 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Cargo Dock</source>
+        <translation>Over Cargo Dock</translation>
+    </message>
+    <message>
+        <source>A two-paned file manager</source>
+        <translation>Een bestandsbeheerder met twee panelen</translation>
+    </message>
+    <message>
+        <source>Cargo Dock is open source software licensed under the terms of the GNU General Public License.</source>
+        <translation>Cargo Dock is vrije software, uitgebracht onder de voorwaarden van de GNU General Public License.</translation>
+    </message>
+    <message>
+        <source>View license</source>
+        <translation>Licentie bekijken</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>Versie %1</translation>
+    </message>
+</context>
+<context>
+    <name>AddServiceDialog</name>
+    <message>
+        <source>Add service</source>
+        <translation>Dienst toevoegen</translation>
+    </message>
+</context>
+<context>
+    <name>DropboxService</name>
+    <message>
+        <source>Login to a Dropbox account.</source>
+        <translation>Meld je aan bij een Dropbox-account.</translation>
+    </message>
+    <message>
+        <source>Configure Dropbox</source>
+        <translation>Dropbox configureren</translation>
+    </message>
+    <message>
+        <source>Enter name</source>
+        <translation>Voer naam in</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Naam</translation>
+    </message>
+    <message>
+        <source>Enter the name under which you want to access your Dropbox folder in Cargo Dock.
+You may add several Dropbox services connected to different Dropbox accounts. The name will help you distinguish them.</source>
+        <translation>Voer de naam in waarmee je je Dropbox-map in Cargo Dock wil benaderen. Je kan verschillende Dropbox-diensten toevoegen, verbonden met verschillende Dropbox-accounts. Dankzij de naam kan je ze beter onderscheiden.</translation>
+    </message>
+</context>
+<context>
+    <name>FileInfoDialog</name>
+    <message>
+        <source>Open</source>
+        <translation>Openen</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Sluiten</translation>
+    </message>
+    <message>
+        <source>Accept</source>
+        <translation>Aanvaarden</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Hernoemen</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Type</translation>
+    </message>
+    <message>
+        <source>Path</source>
+        <translation>Pad</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Grootte</translation>
+    </message>
+    <message>
+        <source>Last modified</source>
+        <translation>Laatst gewijzigd</translation>
+    </message>
+    <message>
+        <source>Modification time</source>
+        <translation>Tijdstip van wijziging</translation>
+    </message>
+    <message>
+        <source>Owner</source>
+        <translation>Eigenaar</translation>
+    </message>
+    <message>
+        <source>Group</source>
+        <translation>Groep</translation>
+    </message>
+    <message>
+        <source>Permissions</source>
+        <translation>Toegangsrechten</translation>
+    </message>
+    <message>
+        <source>Readable</source>
+        <translation>Lezen</translation>
+    </message>
+    <message>
+        <source>Writable</source>
+        <translation>Schrijven</translation>
+    </message>
+    <message>
+        <source>Executable</source>
+        <translation>Uitvoeren</translation>
+    </message>
+    <message>
+        <source>Group permissions</source>
+        <translation>Groepsrechten</translation>
+    </message>
+    <message>
+        <source>World permissions</source>
+        <translation>Wereldrechten</translation>
+    </message>
+</context>
+<context>
+    <name>FolderPage</name>
+    <message>
+        <source>Deleting %1 items</source>
+        <translation>%1 items worden verwijderd</translation>
+    </message>
+    <message>
+        <source>%1 selected</source>
+        <translation>%1 geselecteerd</translation>
+    </message>
+    <message>
+        <source>Pull up for actions</source>
+        <translation>Trek omhoog voor acties</translation>
+    </message>
+    <message>
+        <source>Select some items</source>
+        <translation>Selecteer enkele items</translation>
+    </message>
+    <message>
+        <source>Properties</source>
+        <translation>Eigenschappen</translation>
+    </message>
+    <message>
+        <source>Copy to other side</source>
+        <translation>Kopiëren naar andere kant</translation>
+    </message>
+    <message>
+        <source>Bookmark</source>
+        <translation>Bladwijzer toevoegen</translation>
+    </message>
+    <message>
+        <source>Link to other side</source>
+        <translation>Koppelen naar andere kant</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Verwijderen</translation>
+    </message>
+    <message>
+        <source>All</source>
+        <translation>Alles</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>Geen</translation>
+    </message>
+    <message>
+        <source>New folder ...</source>
+        <translation>Nieuwe map…</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Hulp</translation>
+    </message>
+    <message>
+        <source>Not available</source>
+        <translation>Niet beschikbaar</translation>
+    </message>
+    <message>
+        <source>You have no permission for this folder</source>
+        <translation>Je hebt onvoldoende rechten om deze map te openen</translation>
+    </message>
+    <message>
+        <source>No files</source>
+        <translation>Geen bestanden</translation>
+    </message>
+</context>
+<context>
+    <name>HelpPage</name>
+    <message>
+        <source>Help</source>
+        <translation>Hulp</translation>
+    </message>
+</context>
+<context>
+    <name>LicensePage</name>
+    <message>
+        <source>License</source>
+        <translation>Licentie</translation>
+    </message>
+</context>
+<context>
+    <name>NewFolderDialog</name>
+    <message>
+        <source>Create folder</source>
+        <translation>Map aanmaken</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Naam</translation>
+    </message>
+</context>
+<context>
+    <name>OwnCloudService</name>
+    <message>
+        <source>Access an ownCloud file server.</source>
+        <translation>Verkrijg toegang tot een ownCloud-bestandsserver.</translation>
+    </message>
+    <message>
+        <source>Configure ownCloud</source>
+        <translation>ownCloud configureren</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Naam</translation>
+    </message>
+    <message>
+        <source>Enter name</source>
+        <translation>Voer naam in</translation>
+    </message>
+    <message>
+        <source>Server</source>
+        <translation>Server</translation>
+    </message>
+    <message>
+        <source>Enter server address</source>
+        <translation>Voer serveradres in</translation>
+    </message>
+    <message>
+        <source>Enter path</source>
+        <translation>Voer pad in</translation>
+    </message>
+    <message>
+        <source>Path</source>
+        <translation>Pad</translation>
+    </message>
+    <message>
+        <source>Usually the default path is fine. Edit only if the path of your ownCloud installation differs.</source>
+        <translation>Het standaardpad is meestal goed. Pas dit enkel aan als je ownCloud-installatie een ander pad gebruikt.</translation>
+    </message>
+    <message>
+        <source>Secure connection</source>
+        <translation>Beveiligde verbinding</translation>
+    </message>
+    <message>
+        <source>Not in use</source>
+        <translation>Niet in gebruik</translation>
+    </message>
+    <message>
+        <source>SSL</source>
+        <translation>SSL</translation>
+    </message>
+    <message>
+        <source>Authorization</source>
+        <translation>Autorisatie</translation>
+    </message>
+    <message>
+        <source>Enter login name</source>
+        <translation>Voer naam in</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Naam</translation>
+    </message>
+    <message>
+        <source>Enter password</source>
+        <translation>Voer wachtwoord in</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Wachtwoord</translation>
+    </message>
+</context>
+<context>
+    <name>PassphraseDialog</name>
+    <message>
+        <source>Accept</source>
+        <translation>Aanvaarden</translation>
+    </message>
+    <message>
+        <source>Enter your passphrase to unlock the password storage.</source>
+        <translation>Voer je wachtwoord in om de wachtwoordopslag te ontgrendelen.</translation>
+    </message>
+    <message>
+        <source>Set a passphrase to secure the password storage.
+If you forget the passphrase, your stored passwords will become unusable.</source>
+        <translation>Stel een wachtwoord in om je wachtwoordopslag te beveiligen.
+Als je het wachtwoord vergeet, zullen je opgeslagen wachtwoorden niet meer bruikbaar zijn.</translation>
+    </message>
+    <message>
+        <source>Enter current passphrase to confirm.</source>
+        <translation>Voer je huidige wachtwoord in om te bevestigen.</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>Voer wachtwoord in</translation>
+    </message>
+    <message>
+        <source>Passphrase</source>
+        <translation>Wachtwoord</translation>
+    </message>
+    <message>
+        <source>Verify passphrase</source>
+        <translation>Wachtwoord verifiëren</translation>
+    </message>
+    <message>
+        <source>If you forgot your passphrase, the stored passwords will become unusable and you will have to enter them again.</source>
+        <translation>Als je je wachtwoord vergeet, zullen je opgeslagen wachtwoorden niet meer bruikbaar zijn, en zal je ze opnieuw moeten invoeren.</translation>
+    </message>
+    <message>
+        <source>I forgot</source>
+        <translation>Ik ben het vergeten</translation>
+    </message>
+    <message>
+        <source>Resetting passphrase</source>
+        <translation>Wachtwoord wordt opnieuw ingesteld</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordField</name>
+    <message>
+        <source>Enter password</source>
+        <translation>Voer wachtwoord in</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Wachtwoord</translation>
+    </message>
+</context>
+<context>
+    <name>ServiceObject</name>
+    <message>
+        <source>Configure service</source>
+        <translation>Dienst configureren</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Naam</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Add Service</source>
+        <translation>Dienst toevoegen</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <source>Security</source>
+        <translation>Beveiliging</translation>
+    </message>
+    <message>
+        <source>Secure password storage</source>
+        <translation>Beveiligde wachtwoordopslag</translation>
+    </message>
+    <message>
+        <source>Enable to set a custom passphrase to encrypt your passwords stored in the configuration.
+Your passphrase is not stored and will be asked for once per session.
+If not enabled, passwords are encrypted with a default passphrase.</source>
+        <translation>Schakel dit in om een aangepast wachtwoord in te stellen om je in de configuratie opgeslagen wachtwoorden te versleutelen. Je wachtwoord wordt niet opgeslagen, en zal je één keer per sessie gevraagd worden.
+Indien uitgeschakeld worden wachtwoorden versleuteld met een standaardwachtwoord.</translation>
+    </message>
+    <message>
+        <source>Services</source>
+        <translation>Diensten</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Wordt verwijderd</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Verwijderen</translation>
+    </message>
+    <message>
+        <source>No services yet.
+Drag down to add a service.</source>
+        <translation>Nog geen diensten.
+Trek omlaag om er een toe te voegen.</translation>
+    </message>
+</context>
+<context>
+    <name>SslHandler</name>
+    <message>
+        <source>Accept certificate</source>
+        <translation>Certificaat aanvaarden</translation>
+    </message>
+    <message>
+        <source>Certificate</source>
+        <translation>Certificaat</translation>
+    </message>
+</context>
+<context>
+    <name>TelekomMedienCenter</name>
+    <message>
+        <source>Access Deutsche Telekom® Mediencenter.</source>
+        <translation>Verkrijg toegang tot een Deutsche Telekom®-mediacentrum.</translation>
+    </message>
+</context>
+<context>
+    <name>WebDavService</name>
+    <message>
+        <source>Access a WebDAV share.</source>
+        <translation>Verkrijg toegang tot een gedeelde WebDAV-schijf.</translation>
+    </message>
+    <message>
+        <source>Configure WebDAV</source>
+        <translation>WebDAV configureren</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Naam</translation>
+    </message>
+    <message>
+        <source>Enter name</source>
+        <translation>Voer naam in</translation>
+    </message>
+    <message>
+        <source>Server</source>
+        <translation>Server</translation>
+    </message>
+    <message>
+        <source>Enter server address</source>
+        <translation>Voer serveradres in</translation>
+    </message>
+    <message>
+        <source>Enter path</source>
+        <translation>Voer pad in</translation>
+    </message>
+    <message>
+        <source>Path</source>
+        <translation>Pad</translation>
+    </message>
+    <message>
+        <source>Secure connection</source>
+        <translation>Beveiligde verbinding</translation>
+    </message>
+    <message>
+        <source>Not in use</source>
+        <translation>Niet in gebruik</translation>
+    </message>
+    <message>
+        <source>SSL</source>
+        <translation>SSL</translation>
+    </message>
+    <message>
+        <source>Authorization</source>
+        <translation>Autorisatie</translation>
+    </message>
+    <message>
+        <source>Leave empty if the server requires no authorization.</source>
+        <translation>Laat dit leeg als de server geen autorisatie vereist.</translation>
+    </message>
+    <message>
+        <source>Enter login name</source>
+        <translation>Voer naam in</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Naam</translation>
+    </message>
+    <message>
+        <source>Enter password</source>
+        <translation>Voer wachtwoord in</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Wachtwoord</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-cargodock</name>
+    <message>
+        <source>Pull down for help</source>
+        <translation>Trek omlaag voor hulp</translation>
+    </message>
+    <message>
+        <source>copying</source>
+        <translation>wordt gekopieerd</translation>
+    </message>
+    <message>
+        <source>deleting</source>
+        <translation>wordt verwijderd</translation>
+    </message>
+    <message>
+        <source>linking</source>
+        <translation>wordt gekoppeld</translation>
+    </message>
+</context>
+</TS>
